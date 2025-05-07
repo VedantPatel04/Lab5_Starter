f5b5513c2df440fea4e8a826339410740e6a4d23
lnovilla8 <156245526+lnovilla8@users.noreply.github.com>
2025-05-06 23:42:07 -0700

    fixed sum test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,7 +1,7 @@
 import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 2', () => {
-  expect(1 + 2).toBe(2);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
